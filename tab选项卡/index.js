76f0e209bafbf7b93ae9409773dab6f3a0cddf7d
cnzhhx <33822028+cnzhhx@users.noreply.github.com>
2018-04-10 18:06:55 +0800

    tab选项卡

---
+++ tab选项卡/index.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+
+	var allLis = $('tab_header').getElementsByTagName('li');
+	var allDom = $('tab_content').getElementsByClassName('dom');
+
+
+	for (var i = 0; i < allLis.length; i++) {
+		var li = allLis[i];
+		li.index = i;
+
+		li.onmouseover = function () {
+
+			for (var j = 0; j < allLis.length; j++) {
+				allLis[j].className = '';
+				allDom[j].style.display = 'none';
+			}
+
+			this.className = 'selected';
+			allDom[this.index].style.display = 'block';
+		}
+	}
+	function $(id) {
+		return typeof id === 'string' ? document.getElementById(id) : null;
+	}
+}
